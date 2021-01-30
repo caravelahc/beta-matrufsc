4d8bb0354297bc6b782098d44884c9b5065d6de7
Barichello <artur@barichello.me>
2021-01-30 17:44:45 -0300

    Format python files

--- db/src/get_turmas.py
+++ db/src/get_turmas.py
@@ -11,99 +11,85 @@ import gzip
 import sys
 
 if len(sys.argv) < 1:
-    print('usage: %s [semestre]' % sys.argv[0])
+    print("usage: %s [semestre]" % sys.argv[0])
     sys.exit(1)
 
 try:
     semestre = sys.argv[1]
 except IndexError:
-    semestre = '20182'
+    print("Provide a valid semester as argument")
 
 jar = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar), urllib2.HTTPSHandler(debuglevel=0))
+opener = urllib2.build_opener(
+    urllib2.HTTPCookieProcessor(jar), urllib2.HTTPSHandler(debuglevel=0)
+)
 
-print('Semestre: %s' % semestre)
-#Comentado pois foi encontrado uma forma de acessar as turmmas sem o login
-##print('- Acessando pagina de login')
-#resp = opener.open('https://cagr.sistemas.ufsc.br/modules/aluno')
-#soup = BeautifulSoup(resp)
-#try:
-#    url_action = soup.form['action']
-#    login_form = {}
-#    for input in soup.findAll('input'):
-#        try:
-#            login_form[input['name']] = input['value']
-#        except KeyError:
-#            pass
-#except KeyError:
-#    print 'k-gay'
-#login_form['username'] = sys.argv[1]
-#login_form['password'] = sys.argv[2]
-#
-##print('- Fazendo login')
-#resp = opener.open('https://sistemas.ufsc.br' + url_action, urllib.urlencode(login_form))
-#
+print("Semestre: %s" % semestre)
 
-#print('- Acessando Cadastro de Turmas')
-resp = opener.open('https://cagr.sistemas.ufsc.br/modules/comunidade/cadastroTurmas/')
-soup = BeautifulSoup(resp)
-viewState = soup.find('input', {'name':'javax.faces.ViewState'})['value']
+resp = opener.open("https://cagr.sistemas.ufsc.br/modules/comunidade/cadastroTurmas/")
+soup = BeautifulSoup(resp, features="html.parser")
+viewState = soup.find("input", {"name": "javax.faces.ViewState"})["value"]
 
-#print('- Pegando banco de dados')
-request = urllib2.Request('https://cagr.sistemas.ufsc.br/modules/comunidade/cadastroTurmas/index.xhtml')
-request.add_header('Accept-encoding', 'gzip')
+request = urllib2.Request(
+    "https://cagr.sistemas.ufsc.br/modules/comunidade/cadastroTurmas/index.xhtml"
+)
+request.add_header("Accept-encoding", "gzip")
 page_form = {
-'AJAXREQUEST': '_viewRoot',
-'formBusca:selectSemestre': semestre,
-'formBusca:selectDepartamento': '',
-'formBusca:selectCampus': '1',
-'formBusca:selectCursosGraduacao': '0',
-'formBusca:codigoDisciplina': '',
-'formBusca:j_id135_selection': '',
-'formBusca:filterDisciplina': '',
-'formBusca:j_id139': '',
-'formBusca:j_id143_selection': '',
-'formBusca:filterProfessor': '',
-'formBusca:selectDiaSemana': '0',
-'formBusca:selectHorarioSemana': '',
-'formBusca': 'formBusca',
-'autoScroll': '',
-'javax.faces.ViewState': viewState,
-'formBusca:dataScroller1': '1',
-'AJAX:EVENTS_COUNT': '1',
-        }
+    "AJAXREQUEST": "_viewRoot",
+    "formBusca:selectSemestre": semestre,
+    "formBusca:selectDepartamento": "",
+    "formBusca:selectCampus": "1",
+    "formBusca:selectCursosGraduacao": "0",
+    "formBusca:codigoDisciplina": "",
+    "formBusca:j_id135_selection": "",
+    "formBusca:filterDisciplina": "",
+    "formBusca:j_id139": "",
+    "formBusca:j_id143_selection": "",
+    "formBusca:filterProfessor": "",
+    "formBusca:selectDiaSemana": "0",
+    "formBusca:selectHorarioSemana": "",
+    "formBusca": "formBusca",
+    "autoScroll": "",
+    "javax.faces.ViewState": viewState,
+    "formBusca:dataScroller1": "1",
+    "AJAX:EVENTS_COUNT": "1",
+}
+
 
 def find_id(xml, id):
     for x in xml:
-        if x.get('id') == id:
+        if x.get("id") == id:
             return x
         else:
             y = find_id(x, id)
             if y is not None:
                 return y
     return None
+
+
 def go_on(xml):
-    scroller = find_id(xml, 'formBusca:dataScroller1_table')
+    scroller = find_id(xml, "formBusca:dataScroller1_table")
     if scroller is None:
         return False
     for x in scroller[0][0]:
-        onclick = x.get('onclick')
-        if onclick is not None and 'next' in onclick:
+        onclick = x.get("onclick")
+        if onclick is not None and "next" in onclick:
             return True
     return False
 
-campus_str = [ 'EaD', 'FLO', 'JOI', 'CBS', 'ARA' ]
-if semestre >= '20141':
-    campus_str.append('BLN')
+
+campus_str = ["EaD", "FLO", "JOI", "CBS", "ARA"]
+if semestre >= "20141":
+    campus_str.append("BLN")
 for campus in range(1, len(campus_str)):
-    print('campus ' + campus_str[campus])
-    outfile = open(semestre + '_' + campus_str[campus] + '.xml', 'w')
-    page_form['formBusca:selectCampus'] = campus
+    print("campus " + campus_str[campus])
+    outfile = open(semestre + "_" + campus_str[campus] + ".xml", "w")
+    page_form["formBusca:selectCampus"] = campus
     pagina = 1
     while 1:
-        page_form['formBusca:dataScroller1'] = pagina
+        page_form["formBusca:dataScroller1"] = pagina
         resp = opener.open(request, urllib.urlencode(page_form))
-        if resp.info().get('Content-Encoding') == 'gzip':
+        if resp.info().get("Content-Encoding") == "gzip":
             buf = StringIO(resp.read())
             f = gzip.GzipFile(fileobj=buf)
             data = f.read()
@@ -111,18 +97,20 @@ for campus in range(1, len(campus_str)):
             data = resp.read()
         outfile.write(data)
         parser = ET.XMLParser()
-        parser.entity.update({
-            'aacute': 'á',
-            'atilde': 'ã',
-            'ccedil': 'ç',
-            'eacute': 'é',
-            'ecirc': 'ê',
-            'Aacute': 'Á',
-            'Atilde': 'Ã',
-            'Ccedil': 'Ç',
-            'Eacute': 'É',
-            'Ecirc': 'Ê',
-        })
+        parser.entity.update(
+            {
+                "aacute": "á",
+                "atilde": "ã",
+                "ccedil": "ç",
+                "eacute": "é",
+                "ecirc": "ê",
+                "Aacute": "Á",
+                "Atilde": "Ã",
+                "Ccedil": "Ç",
+                "Eacute": "É",
+                "Ecirc": "Ê",
+            }
+        )
         xml = ET.XML(data, parser=parser)
         if not go_on(xml):
             break
--- db/src/parse_turmas.py
+++ db/src/parse_turmas.py
@@ -13,10 +13,10 @@ import sys
 import os
 
 if len(sys.argv) < 3:
-    print('usage: %s <input> <output>')
+    print("usage: %s <input> <output>")
     sys.exit(1)
 
-os.environ['TZ'] = 'America/Sao_Paulo'
+os.environ["TZ"] = "America/Sao_Paulo"
 
 """newest_db = 0
 for i in range(1, len(sys.argv)-1):
@@ -26,17 +26,16 @@ for i in range(1, len(sys.argv)-1):
     if  newest_db < db_time:
         newest_db = db_time"""
 
-outf = codecs.open(sys.argv[-1], 'w', encoding='utf-8')
-outf.write('{')
-#outf.write('\"DATA\":' + datetime.datetime.fromtimestamp(newest_db).strftime('\"%d/%m/%y - %H:%M\"'))
-outf.write('\"DATA\":' + datetime.datetime.now().strftime('\"%d/%m/%y - %H:%M\"'))
+outf = codecs.open(sys.argv[-1], "w", encoding="utf-8")
+outf.write("{")
+outf.write('"DATA":' + datetime.datetime.now().strftime('"%d/%m/%y - %H:%M"'))
 
-for i in range(1, len(sys.argv)-1):
+for i in range(1, len(sys.argv) - 1):
 
-    outf.write(',')
-    outf.write('\"' + os.path.splitext(sys.argv[i])[0][-3:] + '\":[')
+    outf.write(",")
+    outf.write('"' + os.path.splitext(sys.argv[i])[0][-3:] + '":[')
 
-    inf = open(sys.argv[i], 'r')
+    inf = open(sys.argv[i], "r")
     split = inf.read().split('<?xml version="1.0"?>')
     inf.close()
 
@@ -48,35 +47,35 @@ for i in range(1, len(sys.argv)-1):
         if len(xml) == 0:
             continue
         for row in cElementTree.fromstring(xml)[1][1][2]:
-            codigo_disciplina = row[ 3].text # str
-            nome_turma        = row[ 4].text # str
+            codigo_disciplina = row[3].text  # str
+            nome_turma = row[4].text  # str
 
-            nome_disciplina   = row[ 5].text # str split by <br />
+            nome_disciplina = row[5].text  # str split by <br />
             for sub in row[5]:
-                nome_disciplina = nome_disciplina + ' ' + sub.tail
+                nome_disciplina = nome_disciplina + " " + sub.tail
 
-            horas_aula        = int(row[ 6].text) # int
-            vagas_ofertadas   = int(row[ 7].text) # int
-            vagas_ocupadas    = int(row[ 8].text) # int
-            alunos_especiais  = int(row[ 9].text) # int
+            horas_aula = int(row[6].text)  # int
+            vagas_ofertadas = int(row[7].text)  # int
+            vagas_ocupadas = int(row[8].text)  # int
+            alunos_especiais = int(row[9].text)  # int
             try:
-                saldo_vagas   = int(row[10].text) # int or <span>LOTADA</span>
+                saldo_vagas = int(row[10].text)  # int or <span>LOTADA</span>
             except TypeError:
-                saldo_vagas   = 0
+                saldo_vagas = 0
             try:
-                pedidos_sem_vaga = int(row[11].text) # int or empty
+                pedidos_sem_vaga = int(row[11].text)  # int or empty
             except TypeError:
                 pedidos_sem_vaga = 0
 
-            horarios = []   # str split by <br />, may be emtpy
+            horarios = []  # str split by <br />, may be emtpy
             if row[12].text:
                 horarios.append(row[12].text)
             for sub in row[12]:
                 if sub.tail:
                     horarios.append(sub.tail)
 
-            professores = []    # str split by <br />, may be emtpy, some
-                                # entries may be inside <a>
+            professores = []  # str split by <br />, may be emtpy, some
+            # entries may be inside <a>
             if len(row[13]):
                 if not row[13][0].text:
                     professores.append(row[13].text)
@@ -87,23 +86,39 @@ for i in range(1, len(sys.argv)-1):
                     professores.append(sub.tail)
 
             if codigo_disciplina != prev_codigo:
-                try:    # nome_disciplina may be str or unicode
-                    nome_disciplina_ascii = unicodedata.normalize('NFKD', nome_disciplina).encode('ascii', 'ignore')
+                try:  # nome_disciplina may be str or unicode
+                    nome_disciplina_ascii = unicodedata.normalize(
+                        "NFKD", nome_disciplina
+                    ).encode("ascii", "ignore")
                 except TypeError:
                     nome_disciplina_ascii = nome_disciplina
-                cur_materia = [codigo_disciplina, nome_disciplina_ascii.upper(), nome_disciplina, []]
+                cur_materia = [
+                    codigo_disciplina,
+                    nome_disciplina_ascii.upper(),
+                    nome_disciplina,
+                    [],
+                ]
                 materias.append(cur_materia)
                 prev_codigo = codigo_disciplina
-            turma = [nome_turma, horas_aula, vagas_ofertadas, vagas_ocupadas, alunos_especiais, saldo_vagas, pedidos_sem_vaga, horarios, professores]
+            turma = [
+                nome_turma,
+                horas_aula,
+                vagas_ofertadas,
+                vagas_ocupadas,
+                alunos_especiais,
+                saldo_vagas,
+                pedidos_sem_vaga,
+                horarios,
+                professores,
+            ]
             cur_materia[3].append(turma)
 
     for materia in materias:
-        outf.write(json.dumps(materia, ensure_ascii=False, separators=(',',':')))
-        if materia != materias[len(materias)-1]:
-            outf.write(',')
-        outf.write('\n')
-    outf.write(']')
-#    outf.write(json.dumps(materias, ensure_ascii=False, separators=(',',':')))
+        outf.write(json.dumps(materia, ensure_ascii=False, separators=(",", ":")))
+        if materia != materias[len(materias) - 1]:
+            outf.write(",")
+        outf.write("\n")
+    outf.write("]")
 
-outf.write('}')
+outf.write("}")
 outf.close()
